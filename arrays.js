0bcc13af93d8c4a37ceb463563d15196693cdb28
Wenjie <xuwenjie8866@gmail.com>
2017-10-26 14:17:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
