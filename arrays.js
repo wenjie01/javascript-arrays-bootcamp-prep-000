4ae2238f544778b9cf396da1e1f978e49eb966b3
Wenjie <xuwenjie8866@gmail.com>
2017-10-26 15:15:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,array.length -1);
+}
