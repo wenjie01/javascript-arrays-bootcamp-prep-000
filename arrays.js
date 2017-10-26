e69d4f2b64ddb3bbf366fc5b98989d92c76c05fc
Wenjie <xuwenjie8866@gmail.com>
2017-10-26 14:09:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,8 @@ function destructivelyAddElementToEndOfArray(array,element) {
 
 function accessElementInArray(array,index) {
   return array[index];
+}
 
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
 }
